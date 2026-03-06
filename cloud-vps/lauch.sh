9c6733e73fa535ec957590f71e2f7dc95267a93b
bernardo <bde-albu@42nice.com>
2026-03-06 13:25:25 +0100

    finish

--- cloud-vps/lauch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-qemu-system-x86_64 -m 2G -smp 2 \
-  -enable-kvm \
-  -cpu host \
-  -drive file=cloud1.qcow2,if=virtio \
-  -drive file=seed.iso,format=raw,if=virtio \
-  -net nic -net user,hostfwd=tcp::2222-:22 \
-  -nographic
